954b5fca51039aaaf34fb13ecbec0c8d1cf38402
Martin Rünz <martin.ruenz@posteo.de>
2017-12-13 14:46:55 +0000

    Initial commit.

---
+++ example/example.cpp
@@ -0,0 +1,31 @@
+//#include "../freetype-gl-cpp.h".h"
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(640, 480, "GLyphs", NULL, NULL);
+    if (!window){
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ freetype-gl-cpp.h
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <fontconfig/fontconfig.h>
+
+#include "external/freetype-gl/freetype-gl.h"
+#include "external/freetype-gl/font-manager.h"
+#include "external/freetype-gl/vertex-buffer.h"
+#include "external/freetype-gl/text-buffer.h"
+#include "external/freetype-gl/markup.h"
+#include "external/freetype-gl/demos/shader.h"
+#include "external/freetype-gl/demos/mat4.h"
+#include "external/freetype-gl/screenshot-util.h"
